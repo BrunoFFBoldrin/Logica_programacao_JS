33e1a76212423cd23d9417f34be6a06f7a097352
BrunoFFBoldrin <brunoferreiraboldrin@gmail.com>
2025-08-25 23:58:19 -0300

    1.2 - Função para exibilção dos resultados, validação de numeros repetidos.

--- 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
+++ 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -3,16 +3,19 @@ function sortear() {
  let de = document.getElementById('de').value; // valor mínimo
  let ate = document.getElementById('ate').value; // valor máximo
 
- console.log(quantidade, de, ate); // valores de entrada anexados.
+ console.log(`Valores de entrada: Quantidade de numeros ${quantidade}, minimo ${de}, maximo ${ate}`); // valores de entrada anexados.
 
  numSortidos = []; //Criando array dos numeros sortiados.
 
  for(let i = 0; i < quantidade; i++){
    numSortidos.push(obterNumeroAleatorio(de, ate)); // Adicionando número sorteado ao array, e passando as informações de valores minimos, e maximos.
-
- }
-
+  while(numSortidos.indexOf(numSortidos[i]) != i){ // Verificando se o número já existe no array.
+    numSortidos[i] = obterNumeroAleatorio(de, ate);
+  }
+ let texto = document.getElementById('resultado');
+ texto.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${numSortidos.join(', ')}</label>`;
  console.log(numSortidos); //Validando os numeros sorteados, e sua colocação no array.
+}
 }
 
 function obterNumeroAleatorio(min, max){
@@ -20,5 +23,4 @@ function obterNumeroAleatorio(min, max){
   max = Math.floor(max);// Arredondando para baixo o valor máximo
 
   return Math.floor(Math.random() * (max - min) + min);// Retornando número aleatório
-
 }
