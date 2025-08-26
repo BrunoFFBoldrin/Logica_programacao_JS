c307db63754441c0a3975e0403a4d40a533854bc
BrunoFFBoldrin <brunoferreiraboldrin@gmail.com>
2025-08-25 23:23:54 -0300

    1.1 - Preparando calculos, arrays, e funcoes com as entradas

--- 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
+++ 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -1,7 +1,24 @@
 function sortear() {
- let quantidade = document.getElementById('quantidade').value;
- let de = document.getElementById('de').value;
- let ate = document.getElementById('ate').value;
+ let quantidade = document.getElementById('quantidade').value; // quantidade de números a sortear
+ let de = document.getElementById('de').value; // valor mínimo
+ let ate = document.getElementById('ate').value; // valor máximo
 
- console.log(quantidade, de, ate);
+ console.log(quantidade, de, ate); // valores de entrada anexados.
+
+ numSortidos = []; //Criando array dos numeros sortiados.
+
+ for(let i = 0; i < quantidade; i++){
+   numSortidos.push(obterNumeroAleatorio(de, ate)); // Adicionando número sorteado ao array, e passando as informações de valores minimos, e maximos.
+
+ }
+
+ console.log(numSortidos); //Validando os numeros sorteados, e sua colocação no array.
+}
+
+function obterNumeroAleatorio(min, max){
+  min = Math.ceil(min);// Arredondando para cima o valor mínimo
+  max = Math.floor(max);// Arredondando para baixo o valor máximo
+
+  return Math.floor(Math.random() * (max - min) + min);// Retornando número aleatório
+
 }
