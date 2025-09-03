b7d38590b815a8bc66994012a538624c4ccfa813
BrunoFFBoldrin <brunoferreiraboldrin@gmail.com>
2025-09-02 23:17:07 -0300

    1.7 - Aplicando a funcionalidade para alterçaão das classes dos botões por função.

--- 3411-praticando-logica-projeto_inicial/alugames/js/app.js
+++ 3411-praticando-logica-projeto_inicial/alugames/js/app.js
@@ -6,4 +6,14 @@ function alterarStatus(id) {
     let botaoJogo = gameId.querySelector('.dashboard__item__button');
 
     console.log(nomeJogo.textContent);
+
+    if(imgJogo.classList.contains('dashboard__item__img--rented')) {
+        imgJogo.classList.remove('dashboard__item__img--rented');
+        botaoJogo.classList.remove('dashboard__item__button--return');
+        botaoJogo.textContent = 'Alugar';
+    }else {
+        imgJogo.classList.add('dashboard__item__img--rented');
+        botaoJogo.classList.add('dashboard__item__button--return');
+        botaoJogo.textContent = 'Devolver';
+    }
 }
