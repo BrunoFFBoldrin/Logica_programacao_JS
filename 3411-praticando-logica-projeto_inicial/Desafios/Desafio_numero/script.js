1f725edb04a6060f57bd63695e1650774d071eb8
BrunoFFBoldrin <brunoferreiraboldrin@gmail.com>
2025-09-04 22:06:25 -0300

    2.3 - Acertando configuração do if, adicionando validação de numeros

--- 3411-praticando-logica-projeto_inicial/Desafios/Desafio_numero/script.js
+++ 3411-praticando-logica-projeto_inicial/Desafios/Desafio_numero/script.js
@@ -8,30 +8,38 @@ function organizarNumeros(){
 
 
     console.log(num1, num2, num3);
+    if(num1 < num2 && num1 < num3){
+        if(num2 < num3){
+            alert(`Ordem crescente: ${num1}, ${num2}, ${num3}`);
+        }else{
+            alert(`Ordem crescente: ${num1}, ${num3}, ${num2}`);
+        }if(num2 < num1 && num2 < num3){
+            if(num1 < num3){
+                alert(`Ordem crescente: ${num2}, ${num1}, ${num3}`);
+            }else{
+                alert(`Ordem crescente: ${num2}, ${num3}, ${num1}`);
+            }if(num3 < num1 && num3 < num2){
+                if(num1 < num2){
+                    alert(`Ordem crescente: ${num3}, ${num1}, ${num2}`);
+                }else{
+                    alert(`Ordem crescente: ${num3}, ${num2}, ${num1}`);
+                }
 
-    
-    if(num1 > num2 && num1 > num3) {
-        if(num2 > num3) {
-            alert(`A ordem de maior para menor é: ${num1}, ${num2}, ${num3}`);
-        } else if(num3 > num2) {
-            alert(`A ordem de maior para menor é: ${num1}, ${num3}, ${num2}`);
-
-        } else if(num2 > num1 && num2 > num3) {
-            if(num1 > num3) {
-                alert(`A ordem de maior para menor é: ${num2}, ${num1}, ${num3}`);
-            } else if(num3 > num1) {
-                alert(`A ordem de maior para menor é: ${num2}, ${num3}, ${num1}`);
-
-            }
-        } else if(num3 > num1 && num3 > num2) {
-            if(num1 > num2) {
-                alert(`A ordem de maior para menor é: ${num3}, ${num1}, ${num2}`);
-            } else if(num2 > num1) {
-                alert(`A ordem de maior para menor é: ${num3}, ${num2}, ${num1}`);
-            } else {
-                alert("Cheque os numeros novamente");
             }
         }
-    
+
+    }   
+}
+
+function checaduplicada(){
+    let num1 = parseFloat(document.getElementById('numero1').value);
+    let num2 = parseFloat(document.getElementById('numero2').value);
+    let num3 = parseFloat(document.getElementById('numero3').value);
+
+    if(num1 === num2 || num1 === num3 || num2 === num3) {
+        alert("Números duplicados encontrados!");
+        return
+    }else{
+        organizarNumeros();
     }
 }
