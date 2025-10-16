6c7103b02b3fb76c414fc5e4d6cfa0f1ba70c731
BrunoFFBoldrin <brunoferreiraboldrin@gmail.com>
2025-10-16 00:25:10 -0300

    3.3 - Desafio calculadora - recebendo valores.

---
+++ 3411-praticando-logica-projeto_inicial/Desafios/Desafio_calculadora/js/script.js
@@ -0,0 +1,14 @@
+function capturaValores(){
+let input1 = parseInt(document.getElementById('input1').value);
+let input2 = parseInt(document.getElementById('input2').value);
+let inputSinal = document.getElementById('inputSinal').value;
+
+console.log(input1 , input2, inputSinal);
+
+if(isNaN(input1) || isNaN(input2)){
+    alert('Valores invalidos, por favor, tente novamente.');
+}else{
+    
+}
+
+}
